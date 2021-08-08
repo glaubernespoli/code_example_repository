7d1da49310887f6102810de136f959a882626ca0
glauber.nespoli <glauber.nespoli@gmail.com>
2021-08-08 16:13:49 +0200

    footer

--- tailwindcss-reactjs/src/App.tsx
+++ tailwindcss-reactjs/src/App.tsx
@@ -1,4 +1,5 @@
 import Content from "./components/Content";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Content />
+      <Footer />
     </>
   );
 }
---
+++ tailwindcss-reactjs/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center h-16 border-t border-opacity-60">
+        <p>No Copyrights Ⓐ 2021</p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
